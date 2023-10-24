8bba22187c4042136a99cc583e0f7880bf3df33e
Hazemwaddah <hazem.waddah@live.com>
2023-10-24 15:05:26 +0300

    Add ReadMe file

--- CodiLime-task/variables.tf
+++ CodiLime-task/variables.tf
@@ -1,10 +1,6 @@
 # Global Variables
 
 variable "location" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
 variable "resource_group_name" {}
 variable "virtual_network_name" {}
 variable "subnet_name" {}
