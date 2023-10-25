24da68128adc1c67e2e38130d466f4ae63f18e0d
Hazemwaddah <hazem.waddah@live.com>
2023-10-25 16:05:02 +0300

    create nsg association with vm

--- CodiLime-task/nsg.tf
+++ CodiLime-task/nsg.tf
@@ -16,8 +16,8 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "SSH"
-    priority                   = 1001
+    name                       = "HTTP"
+    priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -26,4 +26,9 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg" {
+  network_interface_id      = azurerm_network_interface.linux.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
