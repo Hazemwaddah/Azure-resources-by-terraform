f3a2ba866073d60cbcf0c277df0a826a656dacfd
Hazemwaddah <hazem.waddah@live.com>
2023-10-25 19:13:44 +0300

    update diagram python architecture.

--- CodiLime-task/Diagram.py
+++ CodiLime-task/Diagram.py
@@ -1,7 +1,14 @@
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.azure.compute import VM
+from diagrams.azure.network import LoadBalancers, VirtualNetworks, NetworkSecurityGroupsClassic
 
-with Diagram("Simple Diagram"):
-    VM("web")
+with Diagram("Azure Architecture"):
+    with Cluster("Resource Group"):
+        vnet = VirtualNetworks("Virtual Network")
+        vm = VM("Virtual Machine")
+        lb = LoadBalancers("Load Balancer")
+        nsg = NetworkSecurityGroupsClassic("NSG")
 
-
+    vnet - vm - nsg - lb
+    #vm - nsg
+    #vm - lb
