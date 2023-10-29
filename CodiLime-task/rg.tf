caac8d0ce52aab8b6ddbc485358adc18cdd67263
Hazemwaddah <hazem.waddah@live.com>
2023-10-29 09:18:07 +0200

    add avilability set

--- CodiLime-task/rg.tf
+++ CodiLime-task/rg.tf
@@ -17,4 +17,13 @@ resource "azurerm_resource_group" "infra" {
   location = var.location
 }
 
+########################################################################################################################
 
+resource "azurerm_availability_set" "avset" {
+  name                         = "avset"
+  location                     = azurerm_resource_group.infra.location
+  resource_group_name          = azurerm_resource_group.infra.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 1
+  managed                      = true
+}
